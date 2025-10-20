00f6aafa806a3e24dc65c9e655e957db96bef13c
shu-one <231462690+shu-one@users.noreply.github.com>
2025-10-21 00:48:06 +0900

    #2 Ktorを導入

---
+++ api/src/main/kotlin/shhashi/ticketManagement/api/Main.kt
@@ -0,0 +1,24 @@
+package shhashi.ticketManagement.api
+
+import io.ktor.server.application.Application
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+import io.ktor.server.tomcat.jakarta.Tomcat
+
+fun main(args: Array<String>) {
+    embeddedServer(
+        Tomcat,
+        port = 8080,
+        module = Application::module,
+    ).start(wait = true)
+}
+
+fun Application.module() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,15 @@
 rootProject.name = "ticket-management"
 
-include("api")
+include(":api")
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+
+    versionCatalogs {
+        create("ktorLibs") {
+            from("io.ktor:ktor-version-catalog:3.3.1")
+        }
+    }
+}
